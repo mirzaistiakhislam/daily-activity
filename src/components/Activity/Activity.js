db2deab91f1c46caaf5a8791c94aa6582227a416
mirzaistiakhislam <mirza15-9603@diu.edu.bd>
2022-09-29 23:29:01 +0600

    clean some unusable codes before submission

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -2,9 +2,6 @@ import React from 'react';
 import './Activity.css';
 
 const Activity = ({ activity, handleAddToCart }) => {
-    // console.log(props);
-    // console.log();
-
     const { picture, name, time } = activity;
     return (
         <div className='activity'>
