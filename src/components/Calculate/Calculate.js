60a553f8d384447cdc77eb452a5c797a1a20c3d1
mirzaistiakhislam <mirza15-9603@diu.edu.bd>
2022-09-29 22:41:05 +0600

    updated calculate part

--- src/components/Calculate/Calculate.js
+++ src/components/Calculate/Calculate.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
-import logo from '../../images/profile.jpg';
+import logo from '../../images/profile1.jpg';
 // import { ToastContainer } from 'react-toastify/dist/components';
 import { addToLocalStorage, getStoredData } from '../../utilities/localstorage';
 import './Calculate.css';
@@ -50,6 +50,7 @@ const Calculate = ({ calculation }) => {
                 </div>
                 <div className='profile-info'>
                     <p>Mirza Istiakh Islam</p>
+                    <span>Dhaka, Bangladesh</span>
                 </div>
             </div>
 
@@ -60,16 +61,16 @@ const Calculate = ({ calculation }) => {
             <div>
                 <h3 className='header-text'>Add A Break</h3>
                 <div className='add-a-break'>
-                    <button onClick={() => { setBreakTime(10); handleBreakTime(10) }} className='ten-seconds'>
+                    <button onClick={() => { setBreakTime(10); handleBreakTime(10) }} className='seconds'>
                         10s
                     </button>
-                    <button onClick={() => { setBreakTime(20); handleBreakTime(20) }} className='ten-seconds'>
+                    <button onClick={() => { setBreakTime(20); handleBreakTime(20) }} className='seconds'>
                         20s
                     </button>
-                    <button onClick={() => { setBreakTime(30); handleBreakTime(30) }} className='ten-seconds'>
+                    <button onClick={() => { setBreakTime(30); handleBreakTime(30) }} className='seconds'>
                         30s
                     </button>
-                    <button onClick={() => { setBreakTime(40); handleBreakTime(40) }} className='ten-seconds'>
+                    <button onClick={() => { setBreakTime(40); handleBreakTime(40) }} className='seconds'>
                         40s
                     </button>
 
@@ -79,15 +80,15 @@ const Calculate = ({ calculation }) => {
             <div>
                 <h3 className='header-text'>Exercise Details</h3>
                 <div className='exercise-time'>
-                    <p><b>Exercise time: {totalTime}</b>seconds</p>
+                    <p><b>Exercise time: {totalTime} </b>seconds</p>
                 </div> <br />
                 <div className='break-time'>
-                    <p><b>Break time: {breakTime}</b>seconds</p>
+                    <p><b>Break time: {breakTime} </b>seconds</p>
                 </div>
             </div>
 
             <div>
-                <button onClick={showingToastWithRoast} className='activity-btn'>Activity Completed</button><ToastContainer />
+                <button onClick={showingToastWithRoast} className='activity-btn'><b>Activity Completed</b></button><ToastContainer />
             </div>
         </div>
     );
